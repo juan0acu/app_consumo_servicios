1007102a871574db0750e3ae7b3883c2d3263f18
juan0acu <juan0acu@gmail.com>
2022-11-25 22:26:10 -0300

    un solo comando

--- app/src/main/java/com/example/appconsumoservicios/MainActivity.kt
+++ app/src/main/java/com/example/appconsumoservicios/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.appconsumoservicios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class MainActivity : AppCompatActivity() {
+    lateinit var text : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        text = findViewById(R.id.text_hola)
         consumirServicio()
     }
 
@@ -22,6 +27,18 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("https://jsonplaceholder.typicode.com").build()
 
         val services = retrofit.create(PostApiServices::class.java)
+        services.getOnePost("1").enqueue(object : Callback<PostModel>{
+            override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
+                println("Respuesta: ${response.body()?.id}" )
+                text.text =response.body()?.title
+            }
+
+            override fun onFailure(call: Call<PostModel>, t: Throwable) {
+                println("Este es un ERROR")
+            }
+
+        })
+/*
         services.getPost().enqueue(object : Callback<PostModel>{
             override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
                 println("Esta es la RESPUESTA")
@@ -35,5 +52,6 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+*/
     }
 }
