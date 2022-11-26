ac0409cabc6c1d1ebb6b4d185c45ced54bab2c0e
juan0acu <juan0acu@gmail.com>
2022-11-25 21:53:41 -0300

    agregado llamado a listado de Post

--- app/src/main/java/com/example/appconsumoservicios/MainActivity.kt
+++ app/src/main/java/com/example/appconsumoservicios/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.appconsumoservicios
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        consumirServicio()
+    }
+
+    fun consumirServicio(){
+
+        val retrofit = Retrofit.Builder() //Instancia para llamar servicio
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://jsonplaceholder.typicode.com").build()
+
+        val services = retrofit.create(PostApiServices::class.java)
+        services.getPost().enqueue(object : Callback<PostModel>{
+            override fun onResponse(call: Call<PostModel>, response: Response<PostModel>) {
+                println("Esta es la RESPUESTA")
+                println(response.body().toString())
+            }
+
+            override fun onFailure(call: Call<PostModel>, t: Throwable) {
+                println("Este es un ERROR")
+                println(t.message)
+
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/appconsumoservicios/PostModel.kt
@@ -0,0 +1,9 @@
+package com.example.appconsumoservicios
+
+
+data class PostModel (
+    val userID: Long,
+    val id: Long,
+    val title: String,
+    val body: String
+)
